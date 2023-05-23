9bdb1bded203eb54a2efd946e8462ce0dbb1a291
Inesh-Reddy <ineshreddy007@gmail.com>
2023-05-23 14:28:50 +0530

    adding  s3 policy

---
+++ sandbox/terraform/s3_access.tf
@@ -0,0 +1,30 @@
+
+# Create an SCP that denies the creation of S3 buckets that starts with 'inception-' name
+resource "aws_organizations_policy" "deny_public_s3_with_name_inception" {
+  content = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Deny",
+        "Action": [
+          "s3:CreateBucket"
+        ],
+        "Resource": [
+        "arn:aws:s3:::inception-*"
+      ]
+      }
+    ]
+  })
+
+
+  description = "Prevent the creation of public S3 buckets"
+  name = "deny_public_s3_with_name_inception-"
+  /* name_prefix = "DenyS3withinception-" */
+  type        = "SERVICE_CONTROL_POLICY"
+}
+/* 
+# Attach the SCP to the root of the organization
+resource "aws_organizations_policy_attachment" "deny_public_s3_with_name_inception_attachment" {
+  policy_id = aws_organizations_policy.deny_public_s3_with_name_inception.id
+  target_id = var.target_id
+} */
